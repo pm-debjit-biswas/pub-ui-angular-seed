a0d24f1e9713ec174ea5241b55a9f37b92dedbec
Debjit Biswas <debjit.biswas@pubmatic.com>
2015-06-16 17:37:12 +0530

    Start server on a random port.
    This allows multiple applications to be started on a single machine.

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,3 +1,4 @@
+var net = require('net');
 var path = require('path');
 var fs = require('fs');
 var chokidar = require('chokidar');
@@ -6,74 +7,90 @@ var http = require('http'),
 
 require('shelljs/global');
 
-cp(
-    '-rf',
-    path.join(__dirname, '../client/index.html'),
-    path.join(__dirname, '../client/200.html')
-);
-
-var watcher = chokidar.watch(path.join(__dirname, 'client/index.html'), {
-    persistent: true
-});
-watcher.on('change', function() {
+function copyIndexTo200() {
     cp(
         '-rf',
         path.join(__dirname, '../client/index.html'),
         path.join(__dirname, '../client/200.html')
     );
-});
-
-var liveServer = require('jspm-server');
-var params = {
-    port: 8282,
-    host: '0.0.0.0',
-    root: 'client',
-    open: false,
-    logLevel: 0
-};
-
-liveServer.start(params);
-
-//
-// Create a proxy server with custom application logic
-//
-var proxy = httpProxy.createProxyServer({});
-
-var proxyConfig = [];
-if (!fs.existsSync('./proxyConfig.json')) {
-    console.log(('You can add proxy configuration, ' +
-        'for APIs, by adding proxyConfig.json file at root').yellow);
-    proxyConfig = [];
-} else {
-    proxyConfig = require(path.join(__dirname, '../proxyConfig.json'));
 }
 
-//
-// Create your custom server and just call `proxy.web()` to proxy
-// a web request to the target passed in the options
-// also you can use `proxy.ws()` to proxy a websockets request
-//
-var server = http.createServer(function(req, res) {
-    // You can define here your custom logic to handle the request
-    // and then proxy the request.
-    var matched = false;
-    proxyConfig.forEach(function (route) {
-        var pattern = Object.keys(route)[0];
-        var match = req.url.match(new RegExp('^/' + pattern + '(.*)'));
-        if (match) {
-            proxy.web(req, res, {target: route[pattern]});
-            matched = true;
+function watchIndex() {
+    var watcher = chokidar.watch(path.join(__dirname, 'client/index.html'), {
+        persistent: true
+    });
+    watcher.on('change', function() {
+        copyIndexTo200();
+    });
+}
+
+function startLiveServer(port) {
+    var liveServer = require('jspm-server');
+    var params = {
+        port: port,
+        host: '0.0.0.0',
+        root: 'client',
+        open: false,
+        logLevel: 0
+    };
+
+    liveServer.start(params);
+
+    return 'http://127.0.0.1:' + port;
+}
+
+function startProxyServer(port, staticServerAddr) {
+
+    var proxy = httpProxy.createProxyServer({});
+
+    var proxyConfig = [];
+    if (!fs.existsSync('./proxyConfig.json')) {
+        console.log(('You can add proxy configuration, ' +
+        'for APIs, by adding proxyConfig.json file at root').yellow);
+        proxyConfig = [];
+    } else {
+        proxyConfig = require(path.join(__dirname, '../proxyConfig.json'));
+    }
+
+    var server = http.createServer(function(req, res) {
+        // You can define here your custom logic to handle the request
+        // and then proxy the request.
+        var matched = false;
+        proxyConfig.forEach(function (route) {
+            var pattern = Object.keys(route)[0];
+            var match = req.url.match(new RegExp('^/' + pattern + '(.*)'));
+            if (match) {
+                proxy.web(req, res, {target: route[pattern]});
+                matched = true;
+            }
+        });
+
+        if (!matched) {
+            proxy.web(req, res, {target: staticServerAddr});
         }
     });
 
-    if (!matched) {
-        proxy.web(req, res, {target: 'http://127.0.0.1:8282'});
-    }
-});
+    server.on('upgrade', function (req, socket, head) {
+        proxy.ws(req, socket, head, {target: staticServerAddr});
+    });
 
-server.on('upgrade', function (req, socket, head) {
-    proxy.ws(req, socket, head, {target: 'http://127.0.0.1:8282'});
-});
+    console.log(('Serving client at http://127.0.0.1:' + port).green);
+    server.listen(port);
+}
 
-console.log(('Listening on port 8181').green);
-server.listen(8181);
+function getRandomPort(cb) {
+    var server = net.createServer().listen(0, function () {
+        var port = server.address().port;
+        server.close(function () {
+            cb(port);
+        });
+    });
+}
+
+getRandomPort(function (staticPort) {
+    var staticServerAddr = startLiveServer(staticPort);
+
+    getRandomPort(function (port) {
+        startProxyServer(port, staticServerAddr);
+    });
+});
