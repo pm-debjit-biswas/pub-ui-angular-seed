4a495fd3ba42e2f30b5fec09b007ce96122e789e
Debjit Biswas <debjit.biswas@pubmatic.com>
2015-06-17 22:32:17 +0530

    Simplify build file code

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 var bundle = require('./bundle');
 var buildHtml = require('./build-html');
-var getVersion = require('./add-version');
+var addVersion = require('./add-version');
 
 require('shelljs/global');
 
@@ -10,26 +10,40 @@ function pathA(file) {
     return path.join(__dirname, '../dist', file);
 }
 
+var minifiedBundle = pathA('bundle.min.js');
+var versionedBundlePattern = pathA('bundle.min.{hash}.js');
+var srcIndexPath = path.join(__dirname, '../client/index.html');
+
 var installedNgVersion = ls('client/jspm_packages/github/angular/*.js')[0]
     .split('@').pop().replace('\.js', '');
+var ngPath = 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/' +
+        installedNgVersion + '/angular.min.js';
+var pmccPath = 'https://s3.amazonaws.com/pubmatic-cc/0.1.39/pmcc.min.js';
 
-bundle('bootstrap', pathA('bundle.min.js'))
-.then(function () {
-    getVersion(pathA('bundle.min.js'), pathA('bundle.min.{hash}.js'), function () {
-        var file = ls(pathA('bundle.min.*.js'))[0]
-            .split('/').pop();
-        var version = file.split('.')[2];
+bundle('bootstrap', minifiedBundle).then(function () {
 
-        rm(pathA('bundle.min.js'));
+    addVersion(minifiedBundle, versionedBundlePattern, function () {
+
+        var versionedBundle = pathA('bundle.min.*.js');
+        var finalBuiltFile = ls(versionedBundle)[0].split('/').pop();
+        var version = finalBuiltFile.split('.')[2];
+
+        rm(minifiedBundle);
+
         cp(pathA('bundle.min.js.map'),
-            pathA('bundle.min.' + version + '.js.map'));
+           pathA('bundle.min.' + version + '.js.map'));
 
-        buildHtml(path.join(__dirname, '../client/index.html'),
-            pathA('index.html'), [
-                'https://cdnjs.cloudflare.com/ajax/libs/angular.js/' +
-                    installedNgVersion + '/angular.min.js',
-                'https://s3.amazonaws.com/pubmatic-cc/0.1.39/pmcc.min.js',
-                file
-            ]);
+        buildHtml(
+            // Input file
+            srcIndexPath,
+            // Output file
+            pathA('index.html'),
+            // Scripts to include
+            [
+                ngPath,
+                pmccPath,
+                finalBuiltFile
+            ]
+        );
     });
 });
