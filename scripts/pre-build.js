82a3738e3145d8959216826727ebd23f6cfa54d1
Debjit Biswas <debjit.biswas@pubmatic.com>
2015-06-17 09:52:58 +0530

    Copy only assets to dist folder

--- scripts/pre-build.js
+++ scripts/pre-build.js
@@ -2,7 +2,8 @@ var path = require('path');
 
 require('shelljs/global');
 
-mkdir('-p', path.join(__dirname, '../dist/app'));
-cp('-r', path.join(__dirname, '../client/app'), path.join(__dirname, '../dist'));
-mkdir(path.join(__dirname, '../dist/client'));
-cp('-r', path.join(__dirname, '../client'), path.join(__dirname, '../dist'));
+mkdir('-p', path.join(__dirname, '../dist/app/assets'));
+cp('-r', path.join(__dirname, '../client/app/assets'), path.join(__dirname, '../dist/app'));
+mkdir('-p', path.join(__dirname, '../dist/client/app/pages'));
+cp('-r', path.join(__dirname, '../client/app/*.js'), path.join(__dirname, '../dist/client/app'));
+cp('-r', path.join(__dirname, '../client/app/pages'), path.join(__dirname, '../dist/client/app'));
