7e3150ca0bd8979ed9ae835bdd4d09b39924ef0b
Debjit Biswas <debjit.biswas@pubmatic.com>
2015-06-19 17:57:51 +0530

    Remove misleading comment

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,8 +1,3 @@
-/**
- * This script is not being used by this example application,
- * but you may use it to do complicated builds in your application.
- */
-
 var jspm = require('jspm');
 
 function bundle(module, outfile) {
