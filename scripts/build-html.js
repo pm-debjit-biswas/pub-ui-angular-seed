8e0834ba0852eab03e596c60d76f77e918a0d8ae
Abhijeet Pawar <abhijeet.pawar@pubmatic.com>
2015-06-21 14:37:26 +0530

    Removed html-dist and ported the code into build-html.js

--- scripts/build-html.js
+++ scripts/build-html.js
@@ -1,9 +1,52 @@
-var HtmlDist = require('html-dist');
+/*********** Code ported from html-dist ************/
+
+var cheerio = require('cheerio');
+// var minify = require('html-minifier').minify;
+
+function HtmlDist(input) {
+    this.input = input;
+    this.$ = cheerio.load(input);
+}
+
+var p = HtmlDist.prototype;
+
+/*p.out = function(shouldMinify) {
+  if(shouldMinify) {
+    return minify(this.$.html(), {
+      collapseWhitespace: true,
+      removeComments: true
+    });
+  } else {
+    return this.$.html();
+  }
+}*/
+
+p.out = function() {
+    return this.$.html();
+};
+
+p.removeAll = function() {
+    this.$('script').remove();
+    return this;
+};
+
+p.insertScript = function(src) {
+    this.$('body').append('<script src="' + src + '"></script>');
+};
+
+p.insertStyle = function(src) {
+    this.$('head').append('<link rel="stylesheet"  href="' + src + '"></link>');
+};
+
+/***************************************************/
 
 require('shelljs/global');
 
-function buildHtml(infile, outfile, scripts) {
+function buildHtml(infile, outfile, scripts, styles) {
     var dist = new HtmlDist(cat(infile));
+
+    scripts = scripts || [];
+    styles = styles || [];
 
     dist.removeAll();
 
@@ -11,7 +54,12 @@ function buildHtml(infile, outfile, scripts) {
         dist.insertScript(script);
     });
 
-    dist.out(true).to(outfile);
+    styles.forEach(function (style) {
+        dist.insertStyle(style);
+    });
+
+    // dist.out(true).to(outfile);
+    dist.out().to(outfile);
 }
 
 module.exports = buildHtml;
